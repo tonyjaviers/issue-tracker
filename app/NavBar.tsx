00670c48c86e54e6996cae25697130b6ca6c71d8
tonyjaviers <tonyjaviers@gmail.com>
2024-08-09 11:17:20 -0400

    Fixed NavBar

---
+++ app/NavBar.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">Logo</Link>
+      <ul className="flex space-x-6">
+        <li>
+          {' '}
+          <Link href="/">Dashboard</Link>
+        </li>
+        <li>
+          {' '}
+          <Link href="/issues">Issues</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
