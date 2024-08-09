c9d1cf5851eb84b1579637fcf8e3dbc69b90b698
tonyjaviers <tonyjaviers@gmail.com>
2024-08-09 14:33:29 -0400

    fixed

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,19 +1,28 @@
+import { link } from 'fs';
 import Link from 'next/link';
 import React from 'react';
+import { AiFillBug } from 'react-icons/ai';
 
 const NavBar = () => {
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues' },
+  ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-      <Link href="/">Logo</Link>
+      <Link href="/">
+        <AiFillBug />
+      </Link>
       <ul className="flex space-x-6">
-        <li>
-          {' '}
-          <Link href="/">Dashboard</Link>
-        </li>
-        <li>
-          {' '}
-          <Link href="/issues">Issues</Link>
-        </li>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
